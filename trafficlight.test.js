50c039f82ab4ebcda21a2ed9212743d57ee2bef1
Tinoxxxmit3x <widmert@bzz.ch>
2025-12-01 14:18:27 +0100

    .

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -1,27 +1,62 @@
-//import module traffic light
-const app = require("./trafficlight");
+//require supertest
+const request = require("supertest");//??
+//url of localserver is assigned to app
+const app = "http://localhost:3000";//??
 
-//test suite
-describe('test suite traffic light', () => {
-    //case red light
-    it(`case red light`, () => {
-        expect(app.trafficLight("red"))
-            .toBe("DON'T WALK!");
+describe("GET /api/welcome", () => {
+    it("welcome message", async () => {
+        return request(app)//??
+            .get("/api/welcome")//??
+            .expect('Content-Type', 'application/json; charset=utf-8')//??
+            .expect('{"message":"Welcome to Traffic-Light!","status":"success"}')//??
+            .then((res) => {//??
+                expect(res.statusCode).toBe(200);//??
+            })//??
     });
-
-    it(`case green light`, () => {
-        expect(app.trafficLight("green"))
-            .toBe("WALK!");
-    })
-
-    it(`case orange light`, () => {
-        expect(app.trafficLight("orange"))
-            .toBe("ATTENTION!");
-    })
-
-    it(`case no light`, () => {
-        expect(app.trafficLight(undefined))
-            .toBe("OUT OF ORDER");
-    });
-
 });
+
+describe("GET /api/trafficlight", () => {
+    it("out of order", async () => {
+        return request(app)//??
+            .get("/api/trafficlight")//??
+            .expect('Content-Type', 'application/json; charset=utf-8')//??
+            //be aware of empty spaces
+            .expect('{"message":"OUT OF ORDER","status":"success"}')//??
+            .then((res) => {//??
+                expect(res.statusCode).toBe(200);//??
+            })//??
+    });
+});
+
+describe("GET /api/trafficlight/:color", () => {
+    it("red", async () => {
+        return request(app)//??
+            .get("/api/trafficlight/red")//??
+            .expect('Content-Type', 'application/json; charset=utf-8')//??
+            .expect(/{"message":"DON'T WALK!","status":"success"}/)//??
+            .then((res) => {//??
+                expect(res.statusCode).toBe(200);//??
+            })//??
+    });
+
+    it("green", async () => {
+        return request(app)//??
+            .get("/api/trafficlight/green")//??
+            .expect('Content-Type', 'application/json; charset=utf-8')//??
+            .expect(/{"message":".*","status":"success"}/)//??
+            .then((res) => {//??
+                expect(res.statusCode).toBe(200);//??
+            })//??
+    });
+
+    it("orange", async () => {
+        return request(app)//??
+            .get("/api/trafficlight/orange")//??
+            .expect('Content-Type', 'application/json; charset=utf-8')//??
+            .expect(/{"message":"ATTENTION!","status":"success"}/)//??
+            .then((res) => {//??
+                expect(res.statusCode).toBe(200);//??
+            })//??
+    });
+});
+
