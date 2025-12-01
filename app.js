e43738e1ce30785f0941f02574bd866b680e1ad5
Tinoxxxmit3x <widmert@bzz.ch>
2025-12-01 13:50:50 +0100

    .

--- app.js
+++
@@ -1,6 +0,0 @@
-const app = require("./trafficlight");
-//Read (parse) argument
-let args = process.argv.slice(2);
-let color = args[0];
-console.log(`Traffic light is ${color}`);
-console.log(app.trafficLight(color));
